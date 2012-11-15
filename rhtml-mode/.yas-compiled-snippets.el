d55e9847eec59920f537f5221aa9a22ca8eb75fb
willnet <netwillnet@gmail.com>
2012-11-15 14:27:07 +0900

    Add password_field

--- rhtml-mode/.yas-compiled-snippets.el
+++ rhtml-mode/.yas-compiled-snippets.el
@@ -12,6 +12,7 @@
                        ("%ifel" "<% if ${cond} %>\n$0\n<% else %>\n<% end %>\n" "<% if  ...  %> $. <% else %> <% end %>" nil nil nil nil nil nil)
                        ("%l" "<%= f.label :${column} %>$0\n" "<%= f.label :body %>" nil nil nil nil nil nil)
                        ("%li" "<%= link_to '${title}', ${path} %>$0" "<%= link_to ' ... ', ' ... ' %>" nil nil nil nil nil nil)
+                       ("%pf" "<%= f.password_field :password %>$0" "<%= f.password_field :password %>" nil nil nil nil nil nil)
                        ("%r" "<%= render '${template}' %>" "<%= render 'form' %>" nil nil nil nil nil nil)
                        ("%s" "<%= f.submit '${string}' %>\n$0\n\n" "<%= f.submit '送信' %>" nil nil nil nil nil nil)
                        ("%ta" "<%= f.text_area :${column} %>$0\n" "<%= f.text_area :body %>" nil nil nil nil nil nil)
@@ -19,4 +20,4 @@
                        ("%un" "<% unless ${cond} %>\n$0\n<% end %>\n" "<% unless  ...  %> $. <% end %>" nil nil nil nil nil nil)))
 
 
-;;; Do not edit! File generated at Wed Nov 14 11:02:21 2012
+;;; Do not edit! File generated at Thu Nov 15 14:24:54 2012
